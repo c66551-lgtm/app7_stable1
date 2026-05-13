49cab2d1328770fd11b6a5253411c9bfcaf47a0c
c66551-lgtm <c66551@gmail.com>
2026-05-13 23:12:23 +0800

    Update app7_stable.py

--- app7_stable.py
+++ app7_stable.py
@@ -911,7 +911,7 @@ def metric_card(title, value, subtext="", accent="#2f80ff", icon=""):
         </div>
 
         <div style="
-            font-size:40px;
+            font-size:30px;
             font-weight:950;
             color:{accent};
             line-height:1.1;
